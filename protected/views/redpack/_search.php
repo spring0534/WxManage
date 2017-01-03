cf65f1568f91867db3f5f835304a37918889b516
tony <tony@aduu.cn>
2017-01-03 15:56:56 +0800

    更换背景图

--- protected/views/redpack/_search.php
+++ protected/views/redpack/_search.php
@@ -9,6 +9,7 @@ $form=$this->beginWidget('CActiveForm', array(
 	)
 ));
 ?>
+	微信昵称:<?php echo $form->textField($model,'nickname',array('class'=>'input-text','style'=>'width:200px;')); ?>&nbsp;
 	订单编号:<?php echo $form->textField($model,'tb_order_no',array('class'=>'input-text','style'=>'width:200px;')); ?>&nbsp;
 	状态: <?php echo CHtml::dropDownList(chtmlName($model, 'status'), $model->status, array(''=>'全部','0'=>'审核失败','1'=>'等待审核','2'=>'派发成功','3'=>'派发失败'))?>
 	<button class="btn primary-bg medium" style="margin-left: 50px;">
